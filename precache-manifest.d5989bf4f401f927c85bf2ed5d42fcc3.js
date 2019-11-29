6dc459b98b4681ff98d4b1cf33b008d47fe7e7df
Andrii <farikpite@gmail.com>
2019-11-30 00:21:29 +0200

    Updates

---
+++ precache-manifest.d5989bf4f401f927c85bf2ed5d42fcc3.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "057e1e53a1c5121f4a7a524f4ff9d843",
+    "url": "/react_uber-eats/index.html"
+  },
+  {
+    "revision": "ae08e73b227af99a9687",
+    "url": "/react_uber-eats/static/css/main.583ab505.chunk.css"
+  },
+  {
+    "revision": "61e63a0fc74d8b4d2a7c",
+    "url": "/react_uber-eats/static/js/2.f6be08f8.chunk.js"
+  },
+  {
+    "revision": "ae08e73b227af99a9687",
+    "url": "/react_uber-eats/static/js/main.fb28684b.chunk.js"
+  },
+  {
+    "revision": "183d5977488a46d51899",
+    "url": "/react_uber-eats/static/js/runtime~main.bef14170.js"
+  }
+]);
